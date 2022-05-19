28f10d854ed528ddd7a1381a99c09e412a35e31a
Subhadipm01 <sbhdpmajumdar@gmail.com>
2022-05-19 21:19:40 +0530

    Second Commit

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 let limit=15
-for(i=1;i<=limit;i++)
-{
+// for(i=1;i<=limit;i++)
+// {
+//     console.log(i)
+// }
+let i=1
+while (i<=limit) {
+    if (i%2==0)
     console.log(i)
+    i++;  
+    
 }
